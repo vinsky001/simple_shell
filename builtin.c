1eef9ad712c66aed1056da66b52e89d66748e01e
vinsky001 <ekibet544@gmail.com>
2023-02-22 05:19:12 -0800

    Simple Shell

--- builtin.c
+++ builtin.c
@@ -6,7 +6,7 @@
    * Return: exits with a given exit status
    * (0) if info.argv[0] != "exit"
    */
-int _myexit(info_t *)
+int _myexit(info_t *info)
 {
 	int exitcheck;
 
@@ -34,7 +34,7 @@ int _myexit(info_t *)
    * constant function prototype.
    * Return: Always 0
    */
-int _mycd(info_t *)
+int _mycd(info_t *info)
 {
 	char *s, *dir, buffer[1024];
 	int chdir_ret;
@@ -84,7 +84,7 @@ int _mycd(info_t *)
    * constant function prototype.
    * Return: Always 0
    */
-int _myhelp(info_t *)
+int _myhelp(info_t *info)
 {
 	char **arg_array;
 
