a46f386a4dd03beb916fb8e8c0fb91f1dc6ed2d8
eva-kaloki <evatindi@gmail.com>
2023-02-22 07:31:29 -0800

    exists updated

--- exits.c
+++ exits.c
@@ -1,6 +1,6 @@
 #include "shell.h"
 /**
- * *_strncpy - copies a string
+ * *_strncpy - copies a string.
  * @dest: the destination string to be copied to
  * @src: the source string
  * @n: the amount of characters to be copied
